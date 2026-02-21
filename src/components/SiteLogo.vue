b9ea6a6fd0fcaaa90e4dc7bf5b834d109752346e
Dejan Vjestica <dvjestica@gmail.com>
2026-02-21 12:54:42 +0100

    add logo

---
+++ src/components/SiteLogo.vue
@@ -0,0 +1,7 @@
+<template>
+  <a aria-label="vuevibe logo to home page"
+    ><img src="../assets/images/logo.png" width="33" height="24"
+  /></a>
+</template>
+
+<style scoped></style>
---
+++ src/components/SiteNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <ul>
+      <li><a>Services</a></li>
+      <li><a>Schedule a Consult</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  ul {
+    display: flex;
+    gap: 3.7rem;
+  }
+}
+</style>
