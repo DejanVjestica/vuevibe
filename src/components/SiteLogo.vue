431e3e2f35920a6e1080cab1cc7a0cdba57e8ff0
Dejan Vjestica <dvjestica@gmail.com>
2026-02-22 17:11:10 +0100

    use class names to selec

--- src/components/SiteLogo.vue
+++ src/components/SiteLogo.vue
@@ -1,11 +1,11 @@
 <template>
-  <a aria-label="vuevibe logo to home page"
+  <a class="logo" href="#/" aria-label="vuevibe logo to home page"
     ><img src="../assets/images/logo.png" width="33" height="24"
   /></a>
 </template>
 
 <style scoped>
-a {
+.logo {
   display: inline-flex;
 }
 </style>
--- src/components/SiteNav.vue
+++ src/components/SiteNav.vue
@@ -1,14 +1,14 @@
 <template>
-  <nav>
+  <nav class="nav">
     <ul>
-      <li><a>Services</a></li>
-      <li><a>Schedule a Consult</a></li>
+      <li><a href="#/Services">Services</a></li>
+      <li><a href="#/Consult">Schedule a Consult</a></li>
     </ul>
   </nav>
 </template>
 
 <style scoped>
-nav {
+.nav {
   display: flex;
   gap: 3.7rem;
   align-items: center;
