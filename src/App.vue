b3a375da17bc5947beaa468a9c92f0d988c53658
Dejan Vjestica <dvjestica@gmail.com>
2026-03-03 09:11:11 +0100

    create plain header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import TheFooter from './components/layout/TheFooter.vue'
 import TheHeader from './components/layout/TheHeader.vue'
+import TheHeaderPlain from './components/layout/TheHeaderPlain.vue'
 </script>
 
 <template>
-  <TheHeader />
+  <TheHeader v-if="!$route.meta.plainLayout" />
+  <TheHeaderPlain v-if="$route.meta.plainLayout" />
   <main class="main">
     <RouterView />
   </main>
-  <TheFooter></TheFooter>
+  <TheFooter v-if="!$route.meta.plainLayout"></TheFooter>
 </template>
---
+++ src/components/layout/TheHeaderPlain.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TheLogo from '../ui/TheLogo.vue'
+</script>
+<template>
+  <header class="header-plain">
+    <TheLogo />
+  </header>
+</template>
+<style scoped>
+.header-plain {
+  grid-template-rows: 10rem;
+  color: var(--color-background-2);
+  display: grid;
+  place-items: center;
+}
+</style>
